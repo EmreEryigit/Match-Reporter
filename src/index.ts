45dae8a449e4042d994640e38631b5c72566f063
Emre Eryiğit <emreeryigit@Emre-MacBook-Air.local>
2022-07-01 01:36:12 +0300

    completed

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import { CsvFileReader } from "./CsvFileReader";
+import { MatchReader } from "./MatchReader";
+import { ConsoleReport } from "./reportTargets/ConsoleReport";
+import { WinsAnalysis } from "./analyzers/WinsAnalysis";
+import { Summary } from "./Summary";
+import { HtmlReport } from "./reportTargets/HtmlReport";
+
+
+const matchReader = MatchReader.fromCsv("football.csv")
+
+matchReader.load();
+
+const summary = Summary.winsAnalysisWithHtmlReport("Man United")
+summary.buildAndPrintReport(matchReader.matches);
